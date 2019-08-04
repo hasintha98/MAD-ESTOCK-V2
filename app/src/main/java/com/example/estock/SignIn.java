98b47330551dc25140c46af4d87c639b110bfaa8
dilkithishaka <dilkithishaka1997@gmail.com>
2019-08-04 18:47:35 +0530

    Merge branch 'master' of https://github.com/hasintha98/MAD-Estock
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    
    redone(dilki) re re

--- app/src/main/java/com/example/estock/SignIn.java
+++ app/src/main/java/com/example/estock/SignIn.java
@@ -1,8 +1,11 @@
 package com.example.estock;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class SignIn extends AppCompatActivity {
 
@@ -10,6 +13,12 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_page);
+
+        TextView Text_signin = findViewById(R.id.Text_signin);
+        Text_signin.setText(R.string.Text_signin);
+
+        EditText Input_uname = findViewById(R.id.Input_uname);
+        Input_uname.setText(R.string.Input_uname);
     }
 }
 
