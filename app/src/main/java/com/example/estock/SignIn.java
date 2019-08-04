32917d254f14e47bb9c118918e653d651e5f9c73
dilkithishaka <dilkithishaka1997@gmail.com>
2019-08-04 18:21:53 +0530

    Merge branch 'master' of https://github.com/hasintha98/MAD-Estock
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    
    redone(dilki) re

---
+++ app/src/main/java/com/example/estock/SignIn.java
@@ -0,0 +1,16 @@
+package com.example.estock;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_page);
+    }
+}
+
+
